f43dec045048668b78ebcdd5d8462349b218c28e
doruk eray <doruk@dorkodu.com>
2022-04-17 19:14:49 +0300

    update all index, css and config files

--- index.jsx
+++ index.jsx
@@ -1,8 +1,14 @@
+//* libraries
 import { Soda } from "@dorkodu/soda";
+import { Superpage } from "@dorkodu/superpage.js";
 
+//* pages
 import { Frontpage } from "./assets/js/pages/Frontpage";
 
+//? add styles with SCSS
 import "./assets/scss/style.scss";
+
+// TODO: add superpage router by-default and add multiple pages
 
 Soda.render(
   <Frontpage />,
