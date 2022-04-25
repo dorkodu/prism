bd943a71c4bebf7b3abb64abdc2e34283feb7873
doruk eray <doruk@dorkodu.com>
2022-04-25 21:08:06 +0300

    update component: Header

--- assets/js/layouts/Header.jsx
+++ assets/js/layouts/Header.jsx
@@ -1,20 +1,24 @@
 import { Soda } from "@dorkodu/soda";
 
-import prismLogo from "../../../images/prism.svg";
+//* components
+import { Image } from "../components/Image";
 
-export function Header() {
+//* images
+import PrismLogo from "../../../images/prism.svg";
+
+export function Header({image, links}) {
   return (
-  <header class="header">
+    <header class="header">
       <div class="__content">
         <div class="__logo">
-          <img src={prismLogo} alt="prism brand" />
+          <Image source={PrismLogo} title="prism logo" />
         </div>
         <nav class="__nav">
           <ul>
             <li><a href="#foreword" rel="dofollow">foreword</a></li>
             <li><a href="#philosophy" rel="dofollow">philosophy</a></li>
             <li><a href="#inspriation" rel="dofollow">inspiration</a></li>
-            <li><a href="https://github.com/dorkodu/prism" rel="dofollow">github</a></li>
+            <li><a href="https://github.com/dorkodu/prism">github</a></li>
             <li><a href="https://dorkodu.com/folks" rel="dofollow">join the folks!</a></li>
           </ul>
         </nav>
