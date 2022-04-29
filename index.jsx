c9bb2c476b5274937f2fb7b3cb8f8fc236faf28d
doruk eray <doruk@dorkodu.com>
2022-04-29 22:03:55 +0300

    integrate superpage.js into prism, with multiple pages

--- index.jsx
+++ index.jsx
@@ -1,16 +1,54 @@
 //* libraries
 import { Soda } from "@dorkodu/soda";
-import { Superpage } from "@dorkodu/superpage.js";
+import { superpage } from "@dorkodu/superpage.js";
 
 //* pages
 import { Frontpage } from "./assets/js/pages/Frontpage";
+import { Foundations } from "./assets/js/pages/Foundations";
+import { Styles } from "./assets/js/pages/Styles";
+import { Components } from "./assets/js/pages/Components";
+import { Oops } from "./assets/js/pages/Oops";
 
 //? add styles with SCSS
 import "./assets/scss/style.scss";
 
-// TODO: add superpage router by-default and add multiple pages
+superpage.route("/", () => {
+  Soda.render(
+    <Frontpage />,
+    document.getElementById("root")
+  );
+});
 
-Soda.render(
-  <Frontpage />,
-  document.getElementById("root")
-);
+superpage.route("foundations", () => {
+  Soda.render(
+    <Foundations/>,
+    document.getElementById("root")
+  );
+});
+
+superpage.route("styles", () => {
+  Soda.render(
+    <Styles/>,
+    document.getElementById("root")
+  );
+});
+
+superpage.route("components", () => {
+  Soda.render(
+    <Components/>,
+    document.getElementById("root")
+  );
+});
+
+superpage.route("oops", () => {
+  Soda.render(
+    <Oops/>,
+    document.getElementById("root")
+  );
+});
+
+superpage.fallback(() => {
+  superpage.to("oops");
+});
+
+superpage.run("hash", () => {});
