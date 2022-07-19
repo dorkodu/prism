c510a151d34394ea589c220e0100acc2b159df0f
doruk eray <doruk@dorkodu.com>
2022-07-19 20:38:59 +0300

    (wip) add scss support
    still does not work, but did these:
    - edit package.json
    - update nuxt.config.ts, aliases etc.
    - add module: @nuxtjs/color-mode
    - rearrange stylesheets
    - update index page

--- website/nuxt.config.ts
+++ website/nuxt.config.ts
@@ -2,5 +2,27 @@ import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-
+  alias: {
+    '~~': '/<rootDir>',
+    '@@': '/<rootDir>',
+    '~': '/<rootDir>',
+    '@': '/<rootDir>',
+    assets: '/<rootDir>/assets',
+    public: '/<rootDir>/public',
+    images: '/<rootDir>/public/images',
+    style: '/<rootDir>/assets/style',
+  },
+  css: ['~/assets/style/style.scss'],
+  // modules
+  modules: ['@nuxtjs/color-mode'],
+  colorMode: {
+    preference: 'system', // default value of $colorMode.preference
+    fallback: 'light', // fallback value if not system preference found
+    hid: 'nuxt-color-mode-script',
+    globalName: '__NUXT_COLOR_MODE__',
+    componentName: 'ColorScheme',
+    classPrefix: '',
+    classSuffix: '-mode',
+    storageKey: 'nuxt-color-mode',
+  },
 })
--- website/pages/index.vue
+++ website/pages/index.vue
@@ -1,24 +1,25 @@
 <script setup>
+import IntroSection1 from '~~/components/IntroSection.vue';
 
 const intro = {
   title: "prism",
   tagline: "our artwork styleguide",
   statement: "we believe in this as a part of our mission, use technology to create wonderful, artistically subtle and liberating experiences for everyone.",
-  picture: <Picture source="~" title="some psychedelics make it work 😉" />,
+  picture: {
+    source: "/images/thewall.jpg",
+    title: "some psychedelics make it work 😉"
+  },
   links: [
-    <LinkButton type="primary" label="our philosophy" link="#philosophy" />,
-    <LinkButton type="secondary" label="join us!" link="https://dorkodu.com/work" />
+
   ]
 };
 
 </script>
 
 <template>
-  <!--
   <div>
-    <IntroSection :title="intro.title" :tagline="our artwork styleguide"
-      statement="we believe in this as a part of our mission, use technology to create wonderful, artistically subtle and liberating experiences for everyone."
-      picture={ } links={, ]} />
+    <IntroSection :title="intro.title" :tagline="intro.tagline" :statement="intro.statement" :picture="intro.picture"
+      :links="intro.links" />
     <article>
       <h1>hi, there 👋</h1>
       <h3>Lorem ipsum dolor sit amet consectetur?</h3>
@@ -29,9 +30,8 @@ const intro = {
       </p>
     </article>
   </div>
-  -->
   <p>hello</p>
 </template>
 
-<style lang="scss" scoped>
+<style lang="scss">
 </style>
