d37bf927b9ee130e25f365d9b68bd2d35c96ecd0
doruk eray <doruk@dorkodu.com>
2022-08-03 10:41:57 +0300

    revise component: Hero
    make Hero component reusable, single column

--- website/components/Hero.vue
+++ website/components/Hero.vue
@@ -1,18 +1,18 @@
 <template>
   <div>
-    <div class="Hero">
-      <div class="__main">
-        <h2 class="__title">{{ title }}</h2>
-        <h1 class="__tagline">{{ tagline }}</h1>
-        <p class="__statement">
+    <div :class="'Hero' + attrs">
+      <div class="__container">
+        <div>
+          <slot name="head"></slot>
+        </div>
+        <h1 class="title">{{ title }}</h1>
+        <h2 class="tagline">{{ tagline }}</h2>
+        <p class="statement">
           {{ statement }}
         </p>
-        <div>
+        <div class="__more">
           <slot name="content"></slot>
         </div>
-      </div>
-      <div class="__side">
-        <slot name="side"></slot>
       </div>
     </div>
   </div>
@@ -20,13 +20,17 @@
 
 <script setup lang="ts">
 
-const { title, tagline, statement }
+const { title, tagline, statement, center }
   = defineProps<{
     title: string,
     tagline: string,
     statement: string,
+    center?: boolean,
   }>();
 
+const attrs = (center)
+  ? " --center"
+  : "";
 </script>
 
 <style lang="scss" scoped>
@@ -36,103 +40,93 @@ const { title, tagline, statement }
   display: flex;
   flex-direction: column;
   align-items: center;
+  justify-content: center;
   gap: 1rem;
-  max-width: $system-layout-breakpoint;
-  margin: 0 auto 2rem;
-  padding: 0 .5rem;
+  max-width: 36rem;
+  margin: 0 auto;
+  padding: 0;
 
-  .__main {
+  .title,
+  .statement,
+  .tagline {
+    text-align: left !important;
+  }
+
+  &.--center {
+    .__container>* {
+      text-align: center !important;
+    }
+  }
+
+  .__container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: left;
+    gap: .5rem;
+
     max-width: 36rem;
     margin: 0 auto;
     padding: 0;
     padding-left: .75rem !important;
     padding-right: .25rem !important;
+
+    >* {
+      text-align: left !important;
+      width: 100% !important;
+    }
   }
 
-  .__side {
-    width: 95%;
-  }
-
-  .__title {
-    margin: 0 auto;
+  .title {
+    display: block !important;
+    margin: 0;
     padding: 0;
     font-size: 1.85rem;
     font-weight: 800;
-    text-align: center;
     color: $reference-color-gray-80;
   }
 
-  .__tagline {
-    margin: .5rem auto;
+  .tagline {
+    margin: 0;
     padding: 0;
     font-size: 1.95rem;
     line-height: 2rem;
     font-weight: 800;
-    text-align: center;
     color: $reference-color-gray-10;
   }
 
-  .__statement {
+  .statement {
+    margin: 0;
     padding: 0;
     font-weight: 600;
     font-size: 1.25rem;
-    margin: .75rem auto;
     line-height: 125%;
-    text-align: center;
     color: $reference-color-gray-60;
   }
 
   p {
     margin-bottom: .5rem;
   }
-}
 
-@media (min-width: $system-viewpoint-mobile-L) {
-  .Hero.--middle {
-    .__main {
-      text-align: center !important;
-    }
-
-    .__title,
-    .__tagline,
-    .__cta,
-    .__statement {
-      text-align: center !important;
-    }
+  .__more {
+    margin-top: .25rem;
   }
 }
 
-@media (min-width: $system-viewpoint-tablet) {}
+@media (mid-width: 900px) {
+  .Hero {
+    .__container {
+      padding-left: 1rem !important;
+    }
+  }
+}
 
 @media (min-width: $system-viewpoint-desktop-S) {
   .Hero {
-    flex-direction: row;
-    padding-left: 1.5rem;
-    padding-right: 1.5rem;
-    gap: 2rem;
-
-    .__main {
-      width: 55%;
-      margin-bottom: 0;
+    .__container {
       text-align: left;
       align-items: flex-start;
-      padding-right: 4rem;
-      flex-grow: 1;
-
-      .__title,
-      .__statement,
-      .__tagline {
-        text-align: left;
-      }
-    }
-
-    .__side {
-      width: 45%;
     }
   }
-}
-
-@media (min-width: 1200px) {
-  .Hero {}
 }
 </style>
