9797cff53947a944ecf03c94e795131a13f2e869
doruk eray <doruk@dorkodu.com>
2022-12-19 22:28:28 +0300

    update prism's app file

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,15 +1,62 @@
 import "./App.css";
-import { MantineProvider } from "@mantine/core";
+import {
+  ActionIcon,
+  ColorScheme,
+  ColorSchemeProvider,
+  MantineProvider,
+  useMantineColorScheme,
+} from "@mantine/core";
 import { theme } from "./theme";
+import { useState } from "react";
+import { IconMoonStars, IconSun } from "@tabler/icons";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
 
 function App() {
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: "mantine-color-scheme",
+    defaultValue: "light",
+    getInitialValueInEffect: true,
+  });
+
+  const dark = colorScheme === "dark";
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
+
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
+
   return (
-    <MantineProvider withGlobalStyles withNormalizeCSS theme={theme}>
-      <div className="App">
-        <h1>Prism UI</h1>
-        <h2>Dorkodu's UI Design System @ Web</h2>
-      </div>
-    </MantineProvider>
+    <ColorSchemeProvider
+      colorScheme={colorScheme}
+      toggleColorScheme={toggleColorScheme}
+    >
+      <MantineProvider
+        theme={{ colorScheme }}
+        withGlobalStyles
+        withNormalizeCSS
+      >
+        <div>
+          <h1>Prism</h1>
+          <h2>Dorkodu's UI Components for the WEB</h2>
+          <div>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ducimus
+              ullam aperiam veniam beatae iure quia facere, odio ipsam tempora
+              ea.
+            </p>
+            <ActionIcon
+              variant="light"
+              color={dark ? "yellow" : "blue"}
+              onClick={() => toggleColorScheme()}
+              title="Toggle color scheme"
+              size={36}
+            >
+              {dark ? <IconSun size={24} /> : <IconMoonStars size={24} />}
+            </ActionIcon>
+          </div>
+        </div>
+      </MantineProvider>
+    </ColorSchemeProvider>
   );
 }
 
